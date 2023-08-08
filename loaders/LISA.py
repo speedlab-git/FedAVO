1bef43c94560665e9095bc7e6f34a5249e7bda3a
hossainzarif <zarifh493@gmail.com>
2023-08-08 15:41:02 -0500

    loaders added

--- loaders/LISA.py
+++ loaders/LISA.py
@@ -28,3 +28,53 @@ def  get_LISA(type="iid", n_samples_train=200, n_samples_test=100, n_clients=3, 
             test=[]
 
         return train, test
+
+def  get_CIFAR(type="iid", n_samples_train=200, n_samples_test=100, n_clients=3, batch_size=25, shuffle=True):
+        dataset_loaded_train = datasets.CIFAR10(
+            root="./data",
+            train=True,
+            download=True,
+            transform=transforms.ToTensor()
+    )
+        dataset_loaded_test = datasets.CIFAR10(
+            root="./data",
+            train=False,
+            download=True,
+            transform=transforms.ToTensor()
+    )
+        if type=="iid":
+            train=iid_split(dataset_loaded_train, n_clients, n_samples_train, batch_size, shuffle)
+            test=iid_split(dataset_loaded_test, n_clients, n_samples_test, batch_size, shuffle)
+        elif type=="non_iid":
+            train=non_iid_split(dataset_loaded_train, n_clients, n_samples_train, batch_size, shuffle)
+            test=non_iid_split(dataset_loaded_test, n_clients, n_samples_test, batch_size, shuffle)
+        else:
+            train=[]
+            test=[]
+
+        return train, test
+    
+def  get_FASHION(type="iid", n_samples_train=200, n_samples_test=100, n_clients=3, batch_size=25, shuffle=True):
+        dataset_loaded_train = datasets.FashionMNIST(
+            root="./data",
+            train=True,
+            download=True,
+            transform=transforms.ToTensor()
+    )
+        dataset_loaded_test = datasets.FashionMNIST(
+            root="./data",
+            train=False,
+            download=True,
+            transform=transforms.ToTensor()
+    )
+        if type=="iid":
+            train=iid_split(dataset_loaded_train, n_clients, n_samples_train, batch_size, shuffle)
+            test=iid_split(dataset_loaded_test, n_clients, n_samples_test, batch_size, shuffle)
+        elif type=="non_iid":
+            train=non_iid_split(dataset_loaded_train, n_clients, n_samples_train, batch_size, shuffle)
+            test=non_iid_split(dataset_loaded_test, n_clients, n_samples_test, batch_size, shuffle)
+        else:
+            train=[]
+            test=[]
+
+        return train, test
